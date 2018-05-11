dc9d57c9c9ebc8dd2c40150e1faa8b1bd6303cd1
Your Name <you@example.com>
2018-05-11 00:03:46 +0000

    instrumenting my code using app insights sdk

--- python_webapp_flask/__init__.py
+++ python_webapp_flask/__init__.py
@@ -6,3 +6,7 @@ from flask import Flask
 app = Flask(__name__)
 
 import python_webapp_flask.views
+
+from applicationinsights.flask.ext import AppInsights
+app.config['APPINSIGHTS_INSTRUMENTATIONKEY'] = '<YOUR INSTRUMENTATION KEY GOES HERE>'
+appinsights = AppInsights(app)
--- python_webapp_flask/views.py
+++ python_webapp_flask/views.py
@@ -5,6 +5,10 @@ Routes and views for the flask application.
 from datetime import datetime
 from flask import render_template
 from python_webapp_flask import app
+from applicationinsights import TelemetryClient
+import sys
+import string
+import random
 
 @app.route('/')
 @app.route('/home')
@@ -35,3 +39,36 @@ def about():
         year=datetime.now().year,
         message='Your application description page.'
     )
+
+@app.route('/track_custom_event')
+def track_custom_event():
+    tc = TelemetryClient(app.config['APPINSIGHTS_INSTRUMENTATIONKEY'])    
+    tc.track_event('ShoppingCartCheckout')
+    tc.flush()    
+    return render_template('index.html')
+
+
+@app.route('/send_metric')
+def send_metric():
+    tc = TelemetryClient(app.config['APPINSIGHTS_INSTRUMENTATIONKEY'])
+    tc.track_metric('queueSize',  random.randint(0,5000))
+    tc.flush()
+    return render_template('index.html')
+
+@app.route('/handledException')
+def handledException():
+    tc = TelemetryClient(app.config['APPINSIGHTS_INSTRUMENTATIONKEY'])
+    try:
+        randomstring = ''.join([random.choice(string.ascii_letters) for n in xrange(8)])
+        raise Exception("Exception {0}".format(randomstring))
+    except:
+        tc.track_exception()
+
+    try:
+        randomstring = ''.join([random.choice(string.ascii_letters) for n in xrange(8)])
+        raise Exception("Exception {0}".format(randomstring))
+    except:
+        tc.track_exception(*sys.exc_info(), properties={ 'foo': 'bar' }, measurements={ 'x': 42 })
+
+    tc.flush()
+    return render_template('index.html')
